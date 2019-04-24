cf98635e2f42349c611f704f2cad87ad43f3126a
willow <shaneweth77@gmail.com>
2019-04-24 11:45:07 -0400

    added prompt for purchase. response is not defined

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,21 +6,39 @@ var con = mysql.createConnection({
     port: 8889,
     user: "root",
     password: "root",
-
     database: "bamazon"
 });
 
-con.connect(function(err) {
+
+con.connect(function (err) {
     if (err) throw err;
     console.log("connected as " + con.threadId);
     displayAll();
+    startPrompt();
 });
 
 function displayAll() {
-    con.query("SELECT * FROM products", function(err, res) {
+    con.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log(res);
-        con.end();
     });
+
+
 }
 
+function startPrompt() {
+    inquirer
+        .prompt({
+            name: "buy",
+            type: "list",
+            message: "Which Item would you like to purchase?",
+            choices: function (err, res) {
+                if (err) throw err;
+                var productArr = [];
+                for (var i = 0; i < res.length; i++) {
+                    productArr.push(res[0].product_name);
+                }
+                return productArr;
+            }
+        })
+}
