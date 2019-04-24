5af547f675f5e21f85766b5dc88eafb25088f027
willow <shaneweth77@gmail.com>
2019-04-24 11:15:35 -0400

    Renamed js file and added a displayAll() function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,26 @@
-const 
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+
+    database: "bamazon"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as " + con.threadId);
+    displayAll();
+});
+
+displayAll() {
+    con.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        con.end();
+    });
+}
+
