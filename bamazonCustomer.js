3c8c6f5f4a57f347a8ef6537c2441122695890d7
willow <shaneweth77@gmail.com>
2019-04-24 17:00:42 -0400

    added formatting for buy function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,28 +19,40 @@ con.connect(function (err) {
 function displayAll() {
     con.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
+        console.log(
+            res.item_id +
+            res.product_name +
+            res.department_name +
+            res.price +
+            res.stock_qty
+        )
     });
     buy();
 }
 
 function buy() {
     inquirer
-    .prompt({
-        name: "buy_item",
-        type: "input",
-        message: "What would you like to buy today, consumer?"
-    })
-    .then(function(answer) {
-        console.log(answer.buy_item);
-        con.query("SELECT * FROM products WHERE ?", { item_id: answer.buy_item }, function(err, res) {
-            console.log(res);
-            con.end();
+        .prompt({
+            name: "buy_item",
+            type: "input",
+            message: "What would you like to buy today, consumer?"
         })
-    })
+        .then(function (answer) {
+            console.log(answer.buy_item);
+            con.query("SELECT * FROM products WHERE ?", { item_id: answer.buy_item }, function (err, res) {
+                console.log(
+                "Item Number: " +   res[0].item_id         + "\n" +
+                "Product: "     +   res[0].product_name    + "\n" +
+                "Department: "  +   res[0].department_name + "\n" +
+                "Price: "       +   res[0].price           + "\n" +
+                "Current QTY: " +   res[0].stock_qty       + "\n" 
+                );
+                con.end();
+            })
+        })
 }
 
 
 
-    
+
 
