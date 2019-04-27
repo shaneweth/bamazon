e3852b7279dd26e76ff128cee0050db1089486bc
willow <shaneweth77@gmail.com>
2019-04-27 12:05:15 -0400

    rearranged buy function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,27 +12,32 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("connected as " + con.threadId);
-    displayAll();
-    buy();
 });
+
+
 
 function displayAll() {
     con.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         
-        console.log("Hello there, fine consumer! Here's a fine list of our fine products: ");
+        console.log(" ");
+        console.log("Hello there, fine consumer! Here's a fine list of our fine products: \n");
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
 
         for (var j = 0; j < res.length; j++) {
-            console.log(
+            console.table(
                 "Item Number: " + res[j].item_id + "\n" +
                 "Product: " + res[j].product_name + "\n" +
                 "Department: " + res[j].department_name + "\n" +
                 "Price: " + res[j].price + "\n" +
-                "Current QTY: " + res[j].stock_qty + "\n"
+                "Current QTY: " + res[j].stock_qty + "\n" +
+                "\n--$$$$$$$$$$$$$$$$$$$$$$$$$$$--\n"
             )
         }
     })
+    buy();    
 }
+displayAll();
 
 function buy() {
     inquirer
@@ -40,7 +45,8 @@ function buy() {
             name: "buy_item",
             type: "input",
             message: "What would you like to buy today, consumer?"
-        }, {
+        }, 
+        {
             name: "quantity",
             type: "input",
             message: "How many of these fine items would you like?",
@@ -60,7 +66,7 @@ function buy() {
 
                 if (res.length === 0) {
                     console.log("ERROR: I'm afraid this doesn't make any sense. Pick something else, please.");
-                    displayAll();
+                    // displayAll();
 
                 } else {
                     const productObj = res[0];
@@ -79,14 +85,7 @@ function buy() {
 
                     }
 
-                    // for (var i = 0; i < res.length; i++) {
-                    //     console.log(
-                    //         "Item Number: " + res[i].item_id + "\n" +
-                    //         "Product: " + res[i].product_name + "\n" +
-                    //         "Department: " + res[i].department_name + "\n" +
-                    //         "Price: " + res[i].price + "\n" +
-                    //         "Current QTY: " + res[i].stock_qty + "\n"
-                    //     )
+                    
                         // tell the database to reduce the res[i].stock_qty by a specific amount
 
                     };
