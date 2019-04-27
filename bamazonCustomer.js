b76aa323091f8ffba65eea6d9cdd3f28c861d13d
willow <shaneweth77@gmail.com>
2019-04-26 21:28:44 -0400

    changes to js. prompt user to purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,7 +26,8 @@ function displayAll() {
                 "Department: " + res[j].department_name + "\n" +
                 "Price: " + res[j].price + "\n" +
                 "Current QTY: " + res[j].stock_qty + "\n"
-            )}
+            )
+        }
     })
 }
 
@@ -36,24 +37,42 @@ function buy() {
             name: "buy_item",
             type: "input",
             message: "What would you like to buy today, consumer?"
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "How many of these fine items would you like?",
         })
         .then(function (answer) {
-            
-            con.query("SELECT * FROM products WHERE ?", { item_id: answer.buy_item }, function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log(
-                        "Item Number: " + res[i].item_id + "\n" +
-                        "Product: " + res[i].product_name + "\n" +
-                        "Department: " + res[i].department_name + "\n" +
-                        "Price: " + res[i].price + "\n" +
-                        "Current QTY: " + res[i].stock_qty + "\n"
-                    )
-                };
+
+            let item = answer.buy_item;
+            let qty = answer.stock_qty;
+
+            var queryStr = "SELECT * products WHERE ?";
+
+            con.query("SELECT * FROM products WHERE ?", {
+                item_id: item
+            }, function (err, res) {
+                if (err) throw err;
+
+                if (res.length === 0) {
+                    console.log("ERROR: I'm afraid this doesn't make any sense. Pick something else, please.");
+                    displayAll();
+
+                } else {
+
+                    for (var i = 0; i < res.length; i++) {
+                        console.log(
+                            "Item Number: " + res[i].item_id + "\n" +
+                            "Product: " + res[i].product_name + "\n" +
+                            "Department: " + res[i].department_name + "\n" +
+                            "Price: " + res[i].price + "\n" +
+                            "Current QTY: " + res[i].stock_qty + "\n"
+                        )
+                        // tell the database to reduce the res[i].stock_qty by a specific amount
+
+                    };
+                }
             })
+
         })
 }
-
-
-
-
-
